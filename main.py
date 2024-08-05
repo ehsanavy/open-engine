61bd9f2b371b07373a25748fc9a6fc6fe2bfd522
ehsanavy <142739696+ehsanavy@users.noreply.github.com>
2024-08-05 12:12:09 +0330

    Update main.py
    edit main

--- main.py
+++ main.py
@@ -6,64 +6,17 @@ from OpenGL.Tk import *
 import sys
 from six.moves import range
 import numpy as np
+import nodes.camera.pct as camera3d
+import dscence
 
 
 
-
-def redraw(o):
-	glClearColor(0, 0.5, 1, 0)
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-	glColor3f(0, 1, 0)
-	#draw checkerboard
-	
-	glColor3f(1 , 0, 0)
-	glPointSize(200.0)
-	
-	glBegin(GL_LINES)		
-	
-	# x line
-	glColor3f(1 , 0, 0)
-	glVertex3fv((-100, 0, 0))
-	glVertex3fv((100, 0, 0))
-	
-	# y line
-	glVertex3fv((0, -100, 0))
-	glVertex3fv((0, 100, 0))
-	
-	# z line
-	glVertex3fv((0, 0, -100))
-	glVertex3fv((0, 0, 100))
-	
-			
-	glEnd()
-	
-	
-	
-	
-	N = 4
-	glDisable(GL_LIGHTING)
-	for x in range(-N, N):
-		for y in range(-N, N):
-			if (x + y) % 2 == 0:
-				glColor3f(1, 1, 1)
-			else:
-				glColor3f(0, 0, 0)
-			#glRectf(x, y, x + 1, y + 1)
-			
-			
-	glEnable(GL_LIGHTING)
-	
-	
-	
+xs = 0
+ys = 0
+zs = 1	
 	
 
-	glPushMatrix()
-	glTranslatef(0., 0., 1.)
-	
-	glutSolidSphere(2.0,30,30)
-	
-	glPopMatrix()
-
+dscence.redraw
 
 def main():
 
@@ -88,7 +41,7 @@ def main():
 	o.set_background( 1.0, 0, 0)
 	o.set_centerpoint(0, 0, 0)
 	glutInit( [] )
-	o.redraw = redraw
+	o.redraw = dscence.redraw
 	
 	quit = Button(f, text = 'Quit', command = sys.exit)
 	quit.pack({'side':'top', 'side':'left'})
